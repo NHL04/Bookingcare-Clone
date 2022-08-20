ed536bfb4f7005490127edafd8cb2a91292989c7
NHL04 <huuluanofficial@gmail.com>
2022-08-20 19:54:55 +0700

    CRUD-U

--- NodeJS/src/controllers/homeController.js
+++ NodeJS/src/controllers/homeController.js
@@ -31,12 +31,38 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log("-------------------");
+    console.log("-----------------------------");
     console.log(data);
-    console.log("-------------------");
+    console.log("-----------------------------");
     return res.render('displayCRUD.ejs', {
         dataTable: data
     })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    console.log(userId);
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        //check user data not found
+
+        // let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('users not found');
+    }
+
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
 }
 
 module.exports = {
@@ -45,4 +71,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
