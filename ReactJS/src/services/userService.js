8765b8c4ad383103b7416863db0dfa3e8ee797f1
NHL04 <huuluanofficial@gmail.com>
2023-03-27 13:20:44 +0700

    editUser-React

--- ReactJS/src/services/userService.js
+++ ReactJS/src/services/userService.js
@@ -23,4 +23,12 @@ const deleteUserService = (userId) => {
     });
 }
 
-export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService }
+const editUserService = (inputData) => {
+    return axios.put('/api/edit-user', inputData);
+}
+
+export {
+    handleLoginApi, getAllUsers,
+    createNewUserService, deleteUserService,
+    editUserService
+}
