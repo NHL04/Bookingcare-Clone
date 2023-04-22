d3b6e5453c527a78139330a4b0a5dd5a571072fe
NHL04 <huuluanofficial@gmail.com>
2023-04-22 21:39:51 +0700

    Admin-Menu-Header

--- ReactJS/src/containers/Header/Header.js
+++ ReactJS/src/containers/Header/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from "../../utils";
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
 
@@ -13,7 +13,7 @@ class Header extends Component {
         this.props.changeLanguageAppRedux(language);
     }
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
 
         return (
             <div className="header-container">
@@ -23,6 +23,10 @@ class Header extends Component {
                 </div>
 
                 <div className="languages">
+                    <span className="welcome">
+                        <FormattedMessage id="homeheader.welcome" />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''} !
+                    </span>
                     <span className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
                     >
@@ -51,6 +55,7 @@ const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
+        userInfo: state.user.userInfo,
     };
 };
 
